4c953e30ebd12fd24d436c98c9f8178916821d9a
ashbirdhiman <38061870+ashbirdhiman@users.noreply.github.com>
2024-01-25 18:52:36 -0500

    Creating base main,go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello Class!\n")
+	var digit int
+	var text string
+	digit = 10
+	text = "Hello"
+	fmt.Printf("%s %d\n", text, digit)
+
+	var varName bool;
+}
